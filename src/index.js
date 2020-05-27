93a0253e4fe7e7551811fdc8a5a8a0eefb2f6ce1
Bryan Rhodunda <rhodunda@udel.edu>
2020-05-27 10:18:24 -0400

    added new cocktail

--- src/index.js
+++ src/index.js
@@ -1,107 +1,179 @@
-const BASE_URL = 'http://localhost:3000/api/v1';
-const COCKTAILS_LINK = BASE_URL + '/cocktails';
-const COCKTAILS_FIRST_CHAR_ARR = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const BASE_URL = "http://localhost:3000/api/v1";
+const COCKTAILS_LINK = BASE_URL + "/cocktails";
+const COCKTAILS_FIRST_CHAR_ARR = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 document.addEventListener("DOMContentLoaded", function () {
-    init()
-})
+  init();
+});
 
 function init() {
-    signUp()
-    login()
-    displayCocktailSelector()
-    // newCocktail()
+  signUp();
+  login();
+  setupSearchForm();
+  displayCocktailSelector();
+  newCocktail();
+}
+
+function setupSearchForm() {
+  const searchForm = document.querySelector("#cocktails-search-form");
+
+  searchForm.addEventListener("submit", fetchCocktails);
+}
+
+function fetchCocktails(e) {
+  e.preventDefault();
+  searchValue = e.target
+    .querySelector("#search-form-input")
+    .value.toLowerCase();
+
+  if (!searchValue) return;
+
+  const fetchObj = {
+    method: "POST",
+  };
+
+  if (e.target.querySelector("#search-option").value == "ingredient")
+    searchUrl = "http://localhost:3000/api/v1/cocktails/search-by-ingredient";
+  else searchUrl = "http://localhost:3000/api/v1/cocktails/search-by-name";
+
+  fetch(`${searchUrl}?q=${searchValue}`, fetchObj)
+    .then((response) => response.json())
+    .then((cocktails) => renderCocktailsTable(cocktails));
+
+  e.target.reset();
 }
 
 function displayCocktailSelector() {
-    const cocktailSelector = document.querySelector('#cocktails-selector');
+  const cocktailSelector = document.querySelector("#cocktails-selector");
 
-    COCKTAILS_FIRST_CHAR_ARR.forEach(char => {
-        cocktailAnchor = document.createElement('a');
-        cocktailAnchor.textContent = ` ${char} `;
-        cocktailAnchor.href = 'javascript:';
-        cocktailAnchor.addEventListener('click', () => { fetchCocktails(char) });
+  COCKTAILS_FIRST_CHAR_ARR.forEach((char) => {
+    cocktailAnchor = document.createElement("a");
+    cocktailAnchor.textContent = ` ${char} `;
+    cocktailAnchor.href = "javascript:";
+    cocktailAnchor.addEventListener("click", () => {
+      fetchCocktailsByChar(char);
+    });
 
-        cocktailSelector.appendChild(cocktailAnchor);
-    })
+    cocktailSelector.appendChild(cocktailAnchor);
+  });
 }
 
-function fetchCocktails(char) {
-    fetch(`http://localhost:3000/api/v1/cocktails?q=${char}`)
-        .then(response => response.json())
-        .then(cocktails => renderCocktailsTable(cocktails));
+function fetchCocktailsByChar(char) {
+  const fetchObj = {
+    method: "POST",
+  };
+  fetch(
+    `http://localhost:3000/api/v1/cocktails/search-by-char?q=${char}`,
+    fetchObj
+  )
+    .then((response) => response.json())
+    .then((cocktails) => renderCocktailsTable(cocktails));
 }
 
 function clearContainerContents(container) {
-    if (container) {
-        while (container.lastChild) {
-            container.lastChild.remove();
-        }
+  if (container) {
+    while (container.lastChild) {
+      container.lastChild.remove();
     }
+  }
 }
 
 function renderCocktailsTable(cocktails) {
-    const container = document.querySelector('#detail');
+  const container = document.querySelector("#detail");
 
-    clearContainerContents(container);
+  clearContainerContents(container);
 
-    const cocktailsTable = buildCocktailsTable(cocktails);
+  const cocktailsTable = buildCocktailsTable(cocktails);
 
-    if (cocktailsTable) {
-        container.appendChild(cocktailsTable);
-    }
+  if (cocktailsTable) {
+    container.appendChild(cocktailsTable);
+  }
 }
 
 function buildCocktailsTable(cocktails) {
-    if (cocktails.length <= 0) {
-        return;
-    }
+  if (cocktails.length <= 0) {
+    return;
+  }
 
-    const table = document.createElement('table');
-    const tHead = document.createElement('thead');
-    const tHeadRow = document.createElement('tr');
+  const table = document.createElement("table");
+  const tHead = document.createElement("thead");
+  const tHeadRow = document.createElement("tr");
 
-    const tHeadName = document.createElement('th');
-    tHeadName.innerText = 'Name';
+  const tHeadName = document.createElement("th");
+  tHeadName.innerText = "Name";
 
-    const tHeadDescription = document.createElement('th');
-    tHeadDescription.innerText = 'Description';
+  const tHeadImage = document.createElement("th");
+  tHeadImage.innerText = "Image";
 
-    const tHeadIngredients = document.createElement('th');
-    tHeadIngredients.innerText = 'Ingredients';
+  const tBody = document.createElement("tbody");
 
-    const tHeadImage = document.createElement('th');
-    tHeadImage.innerText = 'Image';
+  cocktails.forEach((cocktail) => {
+    const tDataRow = document.createElement("tr");
 
-    const tBody = document.createElement('tbody');
+    const tDataName = document.createElement("td");
+    tDataName.innerText = cocktail.name;
 
-    cocktails.forEach(cocktail => {
-        const tDataRow = document.createElement('tr');
+    const tDataImage = document.createElement("td");
+    cocktailImage = document.createElement("img");
+    cocktailImage.src = cocktail.image;
+    cocktailImage.classList.add("table-image");
 
-        const tDataName = document.createElement('td');
-        tDataName.innerText = cocktail.name;
+    tDataImage.appendChild(cocktailImage);
+    tDataRow.append(tDataName, tDataImage);
+    tBody.appendChild(tDataRow);
+  });
 
-        const tDataImage = document.createElement('td');
-        cocktailImage = document.createElement('img');
-        cocktailImage.src = cocktail.image;
-        cocktailImage.classList.add('table-image');
+  tHeadRow.append(tHeadName, tHeadImage);
+  tHead.appendChild(tHeadRow);
+  table.append(tHead, tBody);
 
-        const tDataDescription = document.createElement('td');
-        tDataDescription.innerText = 'Coming soon!';
+  return table;
+}
 
-        const tDataIngredients = document.createElement('td');
-        tDataIngredients.innerText = 'Coming soon!'
+function renderIngredients(cocktail) {
+  ingredientList = document.createElement("ul");
+  cocktail.ingredients.forEach((ingredient) => {
+    ingredientListItem = document.createElement("li");
+    ingredientListItem.innerText = ingredient.name;
+    ingredientList.appendChild(ingredientListItem);
+  });
 
-        tDataImage.appendChild(cocktailImage);
-        tDataRow.append(tDataName, tDataImage, tDataDescription, tDataIngredients);
-        tBody.appendChild(tDataRow);
-    });
-
-    tHeadRow.append(tHeadName, tHeadImage, tHeadDescription, tHeadIngredients);
-    tHead.appendChild(tHeadRow);
-    table.append(tHead, tBody);
-
-    return table;
+  return ingredientList;
 }
--- src/signUp.js
+++ src/signUp.js
@@ -1,70 +1,69 @@
 function signUp() {
-    signUpBtnListener()
+  signUpBtnListener();
 }
 
 function signUpBtnListener() {
-    let signUpBtn = document.querySelector("#sign-up")
-    signUpBtn.addEventListener("click", function (e) {
-        createSignUpForm()
-    })
+  let signUpBtn = document.querySelector("#sign-up");
+  signUpBtn.addEventListener("click", function (e) {
+    createSignUpForm();
+  });
 }
 
 function createSignUpForm() {
-    // clearing detail div
-    let detailsDiv = document.querySelector("#detail")
-    detailsDiv.innerHTML = ''
+  // clearing detail div
+  let detailsDiv = document.querySelector("#detail");
+  detailsDiv.innerHTML = "";
 
-    let signUpH3 = document.createElement('h3')
-    signUpH3.innerText = "Sign-Up"
-    detailsDiv.appendChild(signUpH3)
+  let signUpH3 = document.createElement("h3");
+  signUpH3.innerText = "Sign-Up";
+  detailsDiv.appendChild(signUpH3);
 
-    let signUpForm = document.createElement("form")
-    signUpForm.id = "new-signup-form"
-    detailsDiv.appendChild(signUpForm)
+  let signUpForm = document.createElement("form");
+  signUpForm.id = "new-signup-form";
+  detailsDiv.appendChild(signUpForm);
 
-    let signUpInput1 = document.createElement('input')
-    signUpInput1.type = 'text'
-    signUpInput1.placeholder = 'email'
-    signUpInput1.name = 'email'
-    signUpInput1.autocomplete = 'off'
-    signUpForm.appendChild(signUpInput1)
+  let signUpInput1 = document.createElement("input");
+  signUpInput1.type = "text";
+  signUpInput1.placeholder = "email";
+  signUpInput1.name = "email";
+  signUpInput1.autocomplete = "off";
+  signUpForm.appendChild(signUpInput1);
 
-    let signUpInput2 = document.createElement('input')
-    signUpInput2.type = 'text'
-    signUpInput2.placeholder = 'password'
-    signUpInput2.name = 'password'
-    signUpInput2.autocomplete = 'off'
-    signUpForm.appendChild(signUpInput2)
+  let signUpInput2 = document.createElement("input");
+  signUpInput2.type = "text";
+  signUpInput2.placeholder = "password";
+  signUpInput2.name = "password";
+  signUpInput2.autocomplete = "off";
+  signUpForm.appendChild(signUpInput2);
 
-    let signUpSubmit = document.createElement('input')
-    signUpSubmit.type = "submit"
-    signUpSubmit.value = 'sign up'
-    signUpSubmit.innerText = "Submit"
-    signUpForm.appendChild(signUpSubmit)
+  let signUpSubmit = document.createElement("input");
+  signUpSubmit.type = "submit";
+  signUpSubmit.value = "sign up";
+  signUpSubmit.innerText = "Submit";
+  signUpForm.appendChild(signUpSubmit);
 
-    signUpForm.addEventListener('submit', function (e) {
+  signUpForm.addEventListener("submit", function (e) {
+    let newUser = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
 
-        let newUser = {
-            email: e.target.email.value,
-            password: e.target.password.value
-        }
-
-        creatingUser(newUser)
-        signUpForm.reset()
-    })
+    creatingUser(newUser);
+    signUpForm.reset();
+  });
 }
 
 function creatingUser(newUser) {
-    fetch('http://localhost:3000/api/v1/users', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newUser)
-    })
-        .then(resp => resp.json())
-        .then(data => {
-            sessionStorage.setItem("user_id", `${data.id}`)
-        })
+  fetch("http://localhost:3000/api/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      sessionStorage.setItem("user_id", `${data.id}`);
+      window.alert("Welcome to Cocktail Picker");
+    });
 }
-
